1e9548021868c3b03de4dc6af5bb7682b3781fca
ratchapol-an <ratchapol.an@gmail.com>
2021-02-27 20:24:40 +0700

    update util

--- utils/index.ts
+++ utils/index.ts
@@ -2,5 +2,5 @@
 const numberFormatter = new Intl.NumberFormat('th-TH');
 export const formatAmount = (value: number) => `${numberFormatter.format(value)} บาท`;
 
-const datetimeFormatter = new Intl.DateTimeFormat('th-TH', { month: 'long' });
+const datetimeFormatter = new Intl.DateTimeFormat('th-TH', { day: 'numeric', month: 'long', year: 'numeric' });
 export const formatDate = (value: string) => datetimeFormatter.format(new Date(value));
