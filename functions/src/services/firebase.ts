aa604cada83e22e1786aa3164f54e542a3a22b60
Ratapan Anantawat <ratapan@drivehub.co>
2021-02-27 21:06:11 +0700

    ✨ Add ProductType.

--- functions/src/services/firebase.ts
+++ functions/src/services/firebase.ts
@@ -5,6 +5,8 @@ admin.initializeApp();
 
 export const firebaseFunction = functions.region('asia-southeast2');
 export const db = admin.firestore();
+export const firebaseStorage = admin.storage().bucket('files');
+
 db.settings({ ignoreUndefinedProperties: true });
 export const firebaseAdmin = admin;
 
