d48d59aeff2eaf45023d571f94a5982f6a037c4c
Ratapan Anantawat <ruttaphan21@gmail.com>
2021-03-10 22:35:43 +0700

    ✨  Add Api Search Report.

---
+++ pages/api/reports/get.ts
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Sequelize from 'sequelize';
+import { sequelize as db } from '@db/index';
+import Reports from '@db/report';
+import initAuth from '../../../services/firebaseService';
+
+initAuth();
+enum SearchField {
+  Payment = 'paymentMethod',
+  PhoneNumber = 'phoneNumber',
+  nationalID = 'nationalIdNumber',
+  Name = 'name',
+}
+
+const getSearchQuery = (searchBy: string): string => {
+  switch (searchBy) {
+    case 'bank-account':
+      return SearchField.Payment;
+    case 'phone':
+      return SearchField.PhoneNumber;
+    case 'id-number':
+      return SearchField.nationalID;
+    case 'name':
+      return SearchField.Name;
+    default:
+      return '';
+  }
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'GET') return res.status(403).send('Forbidden!');
+
+  const { q, by } = req.query;
+  console.log(q, by);
+
+  const searchQuery = getSearchQuery(by as string);
+
+  if (searchQuery === '') return res.status(404).send('Missing search by');
+
+  try {
+    const ReportModel = Reports(db, Sequelize);
+    const result = await ReportModel.findAndCountAll({
+      where: {
+        [searchQuery]: q,
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    const totalReport = result.count;
+    let totalDamagedPrice = 0;
+    result.rows.forEach((r) => {
+      totalDamagedPrice += r.getDataValue('amount');
+    });
+
+    return res.status(200).json({
+      name: q,
+      totalReport,
+      totalDamagedPrice,
+      lastedReport: result.rows[0],
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(401).send('Unauthorized');
+  }
+};
+
+export default handler;
