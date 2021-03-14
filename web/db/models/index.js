94ffb55baf104377c5d65b7884b45c0d21f66577
Ratapan Anantawat <ruttaphan21@gmail.com>
2021-03-14 21:36:00 +0700

    🚑

--- web/db/models/index.js
+++ web/db/models/index.js
@@ -6,13 +6,26 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NEXT_PUBLIC_APP_STAGE || 'development';
 const config = require(__dirname + '/../../config/database.json')[env];
+const pg = require('pg');
 let db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize({
+    username: 'TINgkorpHROrpeABle',
+    password: 'fenb642pqfzo1xxl',
+    database: 'check_before_transfer',
+    host: 'sgp-cbtf-do-user-8849046-0.b.db.ondigitalocean.com',
+    port: '25060',
+    ssl: true,
+    dialect: 'postgres',
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false },
+    },
+  });
 }
 
 fs.readdirSync(__dirname)
