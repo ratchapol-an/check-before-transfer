ff5bd8fa53cb89053702ba701a249bd2521f98f7
ratchapol-an <ratchapol.an@gmail.com>
2021-03-18 23:04:20 +0700

    add top progressbar

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -4,6 +4,7 @@ import thTH from 'antd/lib/locale/th_TH';
 import initAuth from 'services/firebaseService';
 import '../styles/antd.less';
 import moment from 'moment';
+import NextJsProgressBar from 'nextjs-progressbar';
 
 initAuth();
 moment.locale('th');
@@ -11,6 +12,7 @@ moment.locale('th');
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ConfigProvider locale={thTH}>
+      <NextJsProgressBar color="#29D" startPosition={0.3} stopDelayMs={200} height={1} />
       <Component {...pageProps} />
     </ConfigProvider>
   );
