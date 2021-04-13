7fc29accb5bad3c121e3e009e63e6f25ebfa7abd
smatipanya <smatipanya@gmail.com>
2021-04-13 09:42:36 +0700

    Update index.tsx

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -13,9 +13,16 @@ export const Home: FunctionComponent = () => {
   return (
     <>
       <Head>
+        
         <title>เช็คคนโกง</title>
         <KeywordsAndDescription />
+
         <link rel="icon" href="/favicon.ico" />
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WR83PLJ');</script>
       </Head>
       <Layout className="home-page-layout layout-with-bg bg-main">
         <Header />
