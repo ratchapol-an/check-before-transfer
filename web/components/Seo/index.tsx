81c11cddbe3ce4b4e1a25046aab519d0d582435f
smatipanya <smatipanya@gmail.com>
2021-04-13 07:22:42 +0700

    gtag

--- web/components/Seo/index.tsx
+++ web/components/Seo/index.tsx
@@ -10,13 +10,6 @@ const KeywordsAndDescription = () => {
         name="keywords"
         content="โอนเงิน,ทรูมันนี่,พร้อมเพย์,truemoney,truemoney wallet,เช็คแม่ค้า,โกงเงิน,เช็คประวัติ,ประวัติคนขาย,เช็คพ่อค้า"
       />
-
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WR83PLJ');</script>
-
     </>
   );
 };
