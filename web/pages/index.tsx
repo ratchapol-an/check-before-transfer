802b34dac57350f7ea88a5e77367e3781105c137
smatipanya <smatipanya@gmail.com>
2021-04-13 09:46:32 +0700

    gtag

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -18,11 +18,7 @@ export const Home: FunctionComponent = () => {
         <KeywordsAndDescription />
 
         <link rel="icon" href="/favicon.ico" />
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WR83PLJ');</script>
+        <script type="text/javascript" src="../config/gtag.js" />
       </Head>
       <Layout className="home-page-layout layout-with-bg bg-main">
         <Header />
