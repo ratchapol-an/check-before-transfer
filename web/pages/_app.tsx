f472a167bdb9668a99f48fa81e45b83b27409a57
smatipanya <disatatk@gmail.com>
2021-04-14 10:54:07 +0700

    SEO

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -21,10 +21,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     authUser.getIdToken().then((token) => {
       if (!token) return;
       const decodedJWT = parseToken(token);
-      const uemail = authUser.email;
-      const uclientInitialized = authUser.clientInitialized;
-      const ufirebaseuid = authUser.firebaseUser.uid;
-      const uid = authUser.id;
     });
   }, [authUser]);
   
@@ -42,13 +38,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <ConfigProvider locale={thTH}>
-      <MetaTags>
-        <meta name="description"
-          content="เช็คคนโกง ก่อนการโอนเงิน จากเลขบัญชีธนาคาร หรือ เบอร์โทรศัพท์มือถือ หรือ เลขประจำตัวประชาชน หรือ ชื่อ-นามสกุล" />
-        <meta
-          name="keywords"
-          content="โอนเงิน,ทรูมันนี่,พร้อมเพย์,truemoney,truemoney wallet,เช็คแม่ค้า,โกงเงิน,เช็คประวัติ,ประวัติคนขาย,เช็คพ่อค้า,คนโกง,เว็บโกง,ร้านโกง,เช็คโกง,กู้เงิน" />
-      </MetaTags>
       <UseGTMHookProvider><p>{authUser.email}</p>
       <NextJsProgressBar color="#00589b" startPosition={0.3} stopDelayMs={200} height={1} />
       </UseGTMHookProvider>
