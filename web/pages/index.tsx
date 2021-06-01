f121f4a7c653fc3ada64eec2cfd9ecc74659693b
smatipanya <82169595+smatipanya@users.noreply.github.com>
2021-06-01 12:24:35 +0700

    modigy SEO metatags

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,8 +6,6 @@ import Container from '@components/Container';
 import Hero from '@components/Hero';
 import { useAuthUser, withAuthUser } from 'next-firebase-auth';
 import './index.less';
-import useGTM from '@elgorditosalsero/react-gtm-hook';
-import SEOTags from '@components/SEO';
 
 export const Home: FunctionComponent = () => {
   const { Content, Footer } = Layout;
@@ -17,27 +15,29 @@ export const Home: FunctionComponent = () => {
       <Head>
       {/* COMMON TAGS */}
       <meta charSet="utf-8" />
-      <title>เช็คก่อนโอน - ลดโอกาสโดนโกงด้วยวิธีง่ายๆ มาเช็คที่นี่ก่อนสิ</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <title>เช็คก่อนโอน - ตรวจสอบให้ชัวร์ ใครขี้โกง บัญชีไหนอันตราย</title>
       {/* Search Engine */}
       <meta name="description" content="โอนเงินได้อย่างมั่นใจ ตัดรายได้มิจฉาชีพ ร่วมสร้างสังคมซื้อขายออนไลน์ที่ปลอดภัยที่เช็คก่อนโอน" />
-      <meta name="image" content="https://firebasestorage.googleapis.com/v0/b/whoscheat-e2261.appspot.com/o/pub%2FSEO-og-image%402x.png?alt=media" />
+      <meta name="image" content="/SEO-og-image-2x.png" />
       {/* Schema.org for Google */}
-      <meta itemProp="name" content="เช็คก่อนโอน - ลดโอกาสโดนโกงด้วยวิธีง่ายๆ มาเช็คที่นี่ก่อนสิ" />
+      <meta itemProp="name" content="เช็คก่อนโอน - ตรวจสอบให้ชัวร์ ใครขี้โกง บัญชีไหนอันตราย" />
       <meta itemProp="description" content="โอนเงินได้อย่างมั่นใจ ตัดรายได้มิจฉาชีพ ร่วมสร้างสังคมซื้อขายออนไลน์ที่ปลอดภัยที่เช็คก่อนโอน" />
-      <meta itemProp="image" content="https://firebasestorage.googleapis.com/v0/b/whoscheat-e2261.appspot.com/o/pub%2FSEO-og-image%402x.png?alt=media" />
+      <meta itemProp="image" content="/SEO-og-image-2x.png" />
       {/* Open Graph general (Facebook, Pinterest & LinkedIn) */}
-      <meta property="og:title" content="เช็คก่อนโอน - ลดโอกาสโดนโกงด้วยวิธีง่ายๆ มาเช็คที่นี่ก่อนสิ" />
+      <meta property="og:title" content="เช็คก่อนโอน - ตรวจสอบให้ชัวร์ ใครขี้โกง บัญชีไหนอันตราย" />
       <meta property="og:description" content="โอนเงินได้อย่างมั่นใจ ร่วมสร้างสังคมซื้อขายออนไลน์ที่ปลอดภัย" />
-      <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/whoscheat-e2261.appspot.com/o/pub%2FSEO-og-image%402x.png?alt=media" />
+      <meta property="og:image" content="/SEO-og-image-2x.png" />
       <meta property="og:url" content="https://www.whoscheat.com" />
       <meta property="og:site_name" content="เช็คก่อนโอน" />
       <meta property="og:locale" content="th-TH" />
       <meta property="og:type" content="website" />
       {/* Twitter */}
       <meta property="twitter:card" content="summary" />
-      <meta property="twitter:title" content="เช็คก่อนโอน - ลดโอกาสโดนโกงด้วยวิธีง่ายๆ มาเช็คที่นี่ก่อนสิ" />
+      <meta property="twitter:title" content="เช็คก่อนโอน - ตรวจสอบให้ชัวร์ ใครขี้โกง บัญชีไหนอันตราย" />
       <meta property="twitter:description" content="โอนเงินได้อย่างมั่นใจ ตัดรายได้มิจฉาชีพ ร่วมสร้างสังคมซื้อขายออนไลน์ที่ปลอดภัยที่เช็คก่อนโอน" />
-      <meta property="twitter:image:src" content="https://firebasestorage.googleapis.com/v0/b/whoscheat-e2261.appspot.com/o/pub%2FSEO-og-image%402x.png?alt=media" />
+      <meta property="twitter:image:src" content="/SEO-og-image-2x.png" />
       </Head>
       <Layout className="home-page-layout layout-with-bg bg-main">
         <Header />
